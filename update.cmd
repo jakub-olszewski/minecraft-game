8faf897ed37a5ccf0fec27c1b99d0c0dfad39b2d
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-05-16 18:23:42 +0200

    update cmd scripts

--- update.cmd
+++ update.cmd
@@ -1,6 +1,7 @@
 @echo off
 title Git update
 
+set project=minecraft-game
 set dev_local=%userprofile%\desktop\dev\
 set git_app=%dev_local%apps\git\bin\git.exe
 set mvn_app=%dev_local%apps\apache-maven-3.5.2\bin\mvn
@@ -10,8 +11,8 @@ echo Current path: %dev_local%
 echo git path: %git_app%
 echo mvn path: %mvn_app%
 
-REM cd /d %dev_local%git\github\minecraft-game
-cd /d %dev_path%
+cd /d %dev_local%git\github\%project%
+REM cd /d %dev_path%
 %git_app% reset --hard HEAD
 %git_app% fetch --all
 %git_app% reset --hard origin/master
