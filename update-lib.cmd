c6046f56581224682ad677569271fc449434f7e6
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-05-16 19:02:18 +0200

    update-lib

--- update-lib.cmd
+++ update-lib.cmd
@@ -19,6 +19,7 @@ echo dev_local path: %dev_local%
 echo desktop_path path: %desktop_path%
 
 title Download from github ...
+mkdir %dev_local%tmp
 cd /d %dev_local%tmp
 rmdir /Q /S %project%
 %git_app% clone https://github.com/jakub-olszewski/%project%.git
@@ -26,8 +27,8 @@ REM cd %project%
 REM cd /d %dev_local%git\github\%project%
 set user_project=%dev_local%git\github\%project%
 set git_project=%dev_local%tmp\%project%
-copy /Y %user_project%\src\main\java\eu\b24u\javafx\minecraft\cube\* %git_project%\src\main\java\eu\b24u\javafx\minecraft\cube
-copy /Y %user_project%\src\main\java\eu\b24u\javafx\minecraft\util\* %git_project%\src\main\java\eu\b24u\javafx\minecraft\util
+copy /Y %git_project%\src\main\java\eu\b24u\javafx\minecraft\cube\* %user_project%\src\main\java\eu\b24u\javafx\minecraft\cube
+copy /Y %git_project%\src\main\java\eu\b24u\javafx\minecraft\util\* %user_project%\src\main\java\eu\b24u\javafx\minecraft\util
 
 timeout /T 3
 exit
